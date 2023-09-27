1d88451e679ec7c8b46334c85493f603a6b741ab
coolman <yangwugui@126.com>
2023-09-27 17:11:46 +0800

    更改了入口文件名：index.esm.js => lit-html.js, 增加了lit-html.d.ts的打包。

--- packages/lit-html/rollup.config.js
+++ packages/lit-html/rollup.config.js
@@ -1,12 +1,17 @@
 import typescript from '@rollup/plugin-typescript';
+import {dts} from 'rollup-plugin-dts'
 
 export default [
   {
     input: 'src/index.ts',
     plugins: [typescript()],
     output: [
-      {file: 'dist/index.js', format: 'cjs'},
-      {file: 'dist/index.esm.js', format: 'es'},
+      {file: 'dist/lit-html.js', format: 'es', sourcemap: true},
     ],
+  },
+  {
+    input: "dist/temp/index.d.ts",
+    output: [{ file: "dist/lit-html.d.ts", format: "es" }],
+    plugins: [dts()],
   },
 ];
